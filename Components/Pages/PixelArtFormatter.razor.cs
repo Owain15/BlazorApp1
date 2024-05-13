a79b7237ca1513a71f91935e093dfeed4094cd33
Owain15 <153022268+Owain15@users.noreply.github.com>
2024-05-13 19:12:52 +0100

    Add project files.

---
+++ Components/Pages/PixelArtFormatter.razor.cs
@@ -0,0 +1,51 @@
+﻿namespace BlazorApp1.Components.Pages
+{
+	
+	
+	public class PixelArtFormatterLogic
+	{
+		
+		private int row;
+	    private int column;
+
+		private int[] pixelReff;
+		
+		//public PixelArtFormatterLogic(int inputRow,int inputColumn)
+		//{
+		//	row = inputRow;
+		//	column = inputColumn;
+
+		//	pixelReff = new int[column*row];
+		//}
+
+		public enum presets
+		{
+			GameBoy,
+			GameBoyAdvanced,
+			NES,
+			SagaGenesis
+		}
+		public enum colorPalette
+		{
+			Black,
+			Aqua,
+			DarkGreen,
+			White
+		}
+
+		// rowUpdate() {
+
+		// }
+
+		
+
+		//int[] pixelArray = GenerateNewIntArray(30,20);
+
+		public int[] NewProject(int column,int row)
+		{ 
+			return new int[column*row]; 
+		}
+		
+
+	}
+}
